882726717f95fd02ddc4b3dbacfd22ffe69d1b20
Jian Li <lijianxp2005@gmail.com>
2015-06-26 01:27:54 -0400

    Problem D: TLE

--- D.cc
+++ D.cc
@@ -18,31 +18,6 @@ void extended_euclidean(int a, int b) {
 } 
 */
 
-#define MAXP    10000
-
-int totalP;
-int prime[MAXP + 1];
-
-void find_prime() {
-    bool flag[MAXP];
-    memset(flag, true, sizeof(flag));
-    for (int i = 2; i <= MAXP; ++ i) {
-        if (!flag[i]) continue;
-        for (int j = i + i; j <= MAXP; j += i)
-            flag[j] = false;
-    }
-    totalP = 0;
-    for (int i = 2; i <= MAXP; ++ i)
-        if (flag[i]) {
-            prime[totalP] = i;
-            ++ totalP;
-        }
-    int k = 0;
-    for (int i = totalP - 1; i >= 0; -- i)
-        if (prime[i] >= 1000) ++ k; else break;
-    printf("%d\n", k);
-}
-
 
 inline int inverse(int b, int MOD){
     int a = MOD, x1 = 0, x2 = 1, q;
@@ -65,44 +40,10 @@ int A[MAXN];
 
 int f[MAXN][MAXN];
 
-
-void preprocess() {
-    find_prime();
-    memset(f[0], 0, sizeof(f[0]));
-    A[0] = A[1] = 1;
-    int n = 1000;
-    for (int pp = 0; pp < totalP; ++ pp) {
-        int p = prime[pp];
-        int l = (p < n ? p : n);
-        for (int i = 1; i < l; ++ i) {
-            long long total = 0;
-            for (int j = 1; j <= i; ++ j) {
-                f[i][j] = f[i - j][j] + A[i + 1 - j];
-                total = total + (long long)j * f[i][j] * A[j];
-            }
-            if (total % i == 0) {
-                total = total / i;
-            }
-            else
-                total = total * inverse(i, p);
-
-            A[i + 1] = total % p;
-        }
-        //ans = A[n];
-        /*
-        for (int i = 1; i <= l; ++ i)
-            printf("%d\n", A[i]);
-        */
-
-    }
-
-}
-
 int main() {
-
-    preprocess();
-    /*
     int k, n, p;
+    //memset(f[0], 0, sizeof(f[0]));
+    A[0] = A[1] = 1;
     while (scanf("%d%d%d", &k, &n, &p) != EOF) {
         int ans;
         int base;
@@ -119,11 +60,26 @@ int main() {
                 if (y & 1) ans = (ans * base) % p;
         }
         else {
-            
+            int i, j;
+            for (i = 1; i < n; ++ i) {
+                long long total = 0;
+                for (j = 1; j <= i; ++ j) {
+                    f[i][j] = f[i - j][j] + A[i + 1 - j];
+                    total = total + (long long)j * f[i][j] * A[j];
+                }
+                if (total % i == 0) {
+                    total = total / i;
+                }
+                else
+                    total = total * inverse(i, p);
+
+                A[i + 1] = total % p;
+            }
+            ans = A[n];
             if (k == 4) {
                 long long tempA = 0;
                 int mid = n >> 1;
-                for (int i = 1; i <= mid; ++ i)
+                for (i = 1; i <= mid; ++ i)
                     tempA = tempA + A[i] * A[n - i];
                 tempA = tempA % p;
                 ans = ans - (int)tempA;
@@ -136,6 +92,5 @@ int main() {
         }
         printf("%d\n", ans);
     }
-    */
     return 0;
 }

eee6daf8d92a40dec447c4976ea9309052deb422
Jian Li <lijianxp2005@gmail.com>
2015-06-30 10:32:03 -0400

    Problem D: Accepted

--- D.cc
+++ D.cc
@@ -1,85 +1,84 @@
-#include <cstring>
 #include <cstdio>
+#include <cstring>
 
 using namespace std;
 
-#define FOR(i, a, b) for (int i=int(a);i<int(b);++i)
-#define REP_1(i, n) for (int i=1;i<=int(n);++i)
+int X, Y;
 
-int MOD;
-
-inline void INC(int &a, int b) { a += b; if (a >= MOD) a -= MOD; }
-inline int ADD(int a, int b) { a += b; if (a >= MOD) a -= MOD; return a; }
-inline void DEC(int &a, int b) { a -= b; if (a < 0) a += MOD; }
-inline void MUL(int &a, int b) { a = a * b % MOD; }
-inline int MULTIPLY(int a, int b) { return a * b % MOD; }
-
-inline int POW(int a, int b) {
-    if (b <= 0) return 1;
-    a %= MOD;
-    int ret = 1;
-    while (b) {
-       if (b & 1) MUL(ret, a); 
-       MUL(a, a), b >>= 1;
+void extended_euclidean(int a, int b) {
+    if (b == 0) {
+        X = 1; Y = 0;
+        return;
     }
-    return ret;
-}
-inline int _inverse(int b){
-    int a = MOD, x1 = 0, x2 = 1, q;
-    while (1){
-        q = a / b, a %= b;
-        if (!a) return (x2 + MOD) % MOD;
-        DEC(x1, MULTIPLY(q, x2));
+    extended_euclidean(b, a % b);
+    int k = X; X = Y; Y = k;
+    Y = Y - (a / b) * X;
+} 
 
-        q = b / a, b %= a;
-        if (!b) return (x1 + MOD) % MOD;
-        DEC(x2, MULTIPLY(q, x1));
-    }
-}
-inline void DIV(int &a, int b) { MUL(a, _inverse(b)); }
-inline int DIVIDE(int a, int b) { return MULTIPLY(a, _inverse(b)); }
-
-const int MAXN = 1001;
+#define MAXN 1000+1
 
 int A[MAXN];
 
 int f[MAXN][MAXN];
-int N, K;
 
 int main() {
-    while (scanf("%d%d%d", &K, &N, &MOD) != EOF) {
-        if (K == 1) {
-            printf("%d\n", POW(N, N - 2));
+    int k, n, p;
+    memset(f[0], 0, sizeof(f[0]));
+    A[0] = A[1] = 1;
+    while (scanf("%d%d%d", &k, &n, &p) != EOF) {
+        int ans;
+        if (k == 1) {
+            ans = 1;
+            int y = n - 2;
+            if (y <= 0)
+                ans = 1;
+            else
+                for (int base = n % p; y; base = (base * base) % p, y >>= 1)
+                    if (y & 1) ans = (ans * base) % p;
         }
-        else if (K == 2) {
-            printf("%d\n", POW(N, N - 1));
-        }
-        else if (K == 3) {
-            memset(A, 0, sizeof(A)); A[1] = 1;
-            FOR(i, 1, N) {
-                REP_1(j, i) {
-                    f[i][j] = ADD(f[i - j][j], A[i + 1 - j]);
-                    INC(A[i + 1], MULTIPLY(MULTIPLY(j, A[j]), f[i][j]));
-                }
-                DIV(A[i + 1], i);
-            }
-            printf("%d\n", A[N]);
+        else if (k == 2) {
+            ans = 1;
+            int y = n - 1;
+            if (y <= 0)
+                ans = 1;
+            else
+                for (int base = n % p; y; base = (base * base) % p, y >>= 1)
+                    if (y & 1) ans = (ans * base) % p;
         }
         else {
-            memset(A, 0, sizeof(A)); A[1] = 1;
-            FOR(i, 1, N) {
-                REP_1(j, i){
-                    f[i][j] = ADD(f[i - j][j], A[i + 1 - j]);
-                    INC(A[i + 1], MULTIPLY(MULTIPLY(j, A[j]), f[i][j]));
+            for (int i = 1; i < n; ++ i) {
+                long long total = 0;
+                for (int j = 1; j <= i; ++ j) {
+                    f[i][j] = f[i - j][j] + A[i + 1 - j];
+                    total = total + (long long)j * f[i][j] * A[j];
                 }
-                DIV(A[i + 1], i);
+                if (total % i == 0) {
+                    total = total / i;
+                }
+                else {
+                    extended_euclidean(i, p);
+                    while (X < 0) X += p;
+                    total = total * X;
+                }
+
+                A[i + 1] = total % p;
             }
-            int ans = A[N];
-            REP_1(i, N >> 1)
-                DEC(ans, MULTIPLY(A[i], A[N - i]));
-            if (!(N & 1))
-                INC(ans, (((A[N / 2] + 1) * A[N / 2]) / 2) % MOD);
-            printf("%d\n", ans);
+            ans = A[n];
+            if (k == 4) {
+                long long tempA = 0;
+                int mid = n >> 1;
+                for (int i = 1; i <= mid; ++ i)
+                    tempA = tempA + A[i] * A[n - i];
+                tempA = tempA % p;
+                ans = ans - (int)tempA;
+                if (ans < 0) ans += p;
+                if ((n & 1) == 0) {
+                    int tempB = ((A[mid] + 1) * A[mid]) >> 1;
+                    ans = (ans + tempB) % p;
+                }
+            }
         }
+        printf("%d\n", ans);
     }
+    return 0;
 }
